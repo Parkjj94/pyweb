49fc1e094acf4dff802a0778741a51837a0601cc
Parkjj94 <ind01158@naver.com>
2021-12-22 12:30:06 +0900

    common

---
+++ common/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+
+from common.forms import UserForm
+
+
+def signup(request):
+    #회원 가입
+    if request.method == "POST":
+        form = UserForm(request.POST)  # 입력값을 가져옴
+        if form.is_valid():
+            form.save()     # 실제 저장
+            return redirect('board:index')
+    else:
+        form = UserForm()   # 비어있는 새폼
+    return render(request, 'common/signup.html', {'form':form})
